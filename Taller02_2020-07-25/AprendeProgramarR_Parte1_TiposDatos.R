ac3d96a18328c77f7ecd1ae6658cdafbfe2bca99
Denisse Fierro Arcos <lidefi87@hotmail.com>
2020-07-19 21:25:33 -0500

    Script finalizado para el taller

--- Taller02_2020-07-25/AprendeProgramarR_Parte1_TiposDatos.R
+++ Taller02_2020-07-25/AprendeProgramarR_Parte1_TiposDatos.R
@@ -1,14 +1,13 @@
 #Taller: Aprende a programar en R - Parte 1: Tipos de datos
 #Autor: Denisse Fierro Arcos, R-Ladies Galapagos
 #Fecha: 2020-07-19
-#Detalles: En este script veremos los diferentes tipos de datos que podemos manipular en R, así
-#como practicar crear variables
+#Detalles: En este script veremos los diferentes tipos de datos que podemos manipular
+#en R, así #como practicar crear variables
 
 
 # Caracteres --------------------------------------------------------------
 "Bobby"
 "Lisa"
-
 
 # Asignando caracter a una variable ---------------------------------------
 H <- "Bobby"
@@ -24,6 +23,9 @@ class(A)
 assign("Nombre", "Lisa")
 Nombre
 class(Nombre)
+
+#Borrar variables
+rm(H, M, A, Nombre)
 
 # Datos numéricos ---------------------------------------------------------
 #Incluye enteros y decimales
@@ -89,6 +91,7 @@ y
 y <- y/-x
 y
 
+rm(m, x, y, z)
 
 # Números complejos -------------------------------------------------------
 #Los números complejos están representados por un número real y un número imaginario
@@ -188,6 +191,8 @@ z
 class(z)
 z*5
 
+rm(x, y, z, x1, x2)
+
 #Creando un vector repitiendo un número o variable
 x <- rep(10, times = 4)
 x
@@ -204,3 +209,252 @@ x; y; z
 nuevo <- c(x, y, z)
 nuevo
 length(nuevo)
+
+nuevo2 <- c(x, rep(y, 3))
+nuevo2
+
+nuevo3 <- c(y, rep(10:14, each = 3))
+nuevo3
+
+#Uso de booleanos
+nuevo3 > 10
+nuevo3 < 10
+nuevo3 != 10
+
+rm(nuevo, nuevo2, nuevo3)
+
+#Operaciones con vectores
+x <- seq(from = 5, to = 15, by = 5)
+x
+x*5
+(10*x)^2
+x*pi
+sqrt(x)
+x * c(1:3)
+x * c(1:2)
+
+rm(x, y, z)
+
+# Vectores con caracteres -------------------------------------------------
+nombres <- c("Laura", "Pedro", "Cecilia", "Ximena")
+length(nombres)
+
+#Accediendo elementos específicos de un vector
+nombres[4]
+nombres[1:2]
+nombres[c(1,3)]
+nombres[c(1,3:4)]
+nombres[-2]
+
+#Cambiando un elemento
+nombres[4] <- "Jorge"
+nombres
+#Aumentando un elemento
+nombres[5] <- "Marina"
+nombres
+nombres[6:7] <- c("Isabel", "Estela")
+nombres
+nombres[9] <- "Julio"
+nombres
+#NA significa que ese elemento no tiene un valor asignado
+rm(nombres)
+
+#Podemos hacer lo mismo con vectores numéricos
+x <- seq(from = 5, to = 15, by = 5)
+x
+x[1] <- 30
+x
+
+
+# Matrices ----------------------------------------------------------------
+#Creando una matriz basada en una sequencia
+#Podemos definit el numero de filas
+x <- matrix(1:30, nrow = 5)
+x
+length(x)
+dim(x)
+class(x)
+#O el numero de columnas
+y <- matrix(seq(2,100, by = 5), ncol = 10)
+y
+dim(y)
+#O ambos
+z <- matrix(rep(10, 20), ncol = 5, nrow = 4)
+z
+#Podemos definir si la matriz es llenada por columnas o filas
+matrix(1:30, nrow = 5, byrow = FALSE) #por columna
+matrix(1:30, nrow = 5, byrow = T) #por file
+
+#Podemos crear matrices basadas en vectores existentes
+x <- seq(1:50)
+x
+xMat <- matrix(x, nrow = 10) #ordenadas por columna
+xMat
+xMatFila <- matrix(x, nrow = 10, byrow = T) #ordenadas por fila
+xMatFila
+
+#Podemos combinar matrices para crear matrices mas grandes
+#Una sobre otra con rbind()
+xFila <- rbind(xMat, xMatFila)
+xFila
+#Una al lado de la otra con cbind()
+xCol <- cbind(xMat, xMatFila)
+xCol
+
+
+# Cambiando el nombre de columnas y filas ---------------------------------
+xCol
+colnames(xCol)
+colnames(xCol) <- c("Uno", "Dos", "Tres", "Cuatro", "Cinco", "Seis",  "Siete",
+                    "Ocho", "Nueve", "Diez")
+colnames(xCol)
+xCol
+
+rownames(xCol)
+rownames(xCol) <- c("uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", 
+                    "ocho", "nueve", "diez")
+rownames(xCol)
+xCol
+
+
+# Accediendo elementos de la matriz ---------------------------------------
+#Por filas
+xFila
+xFila[1,]
+xFila[1:3,]
+xFila[c(1, 5),]
+xCol["uno",]
+
+#Por columna
+xFila[,1]
+xFila[,3:5]
+xFila[,c(2, 4)]
+xCol[,"Uno"]
+
+#Por elemento
+xFila[1,5]
+xFila[15,3]
+xCol["cuatro", "Tres"]
+
+#Podemos reemplazar los valores de elementos específicos
+xCol
+xCol[1,] <- 3
+xCol
+xCol[,1] <- rep(1:5, 2)
+xCol
+xCol[6,10] <- 100
+xCol
+xCol["cuatro", "Tres"] <- 350
+xCol
+
+#Podemos también cambiar el nombre de una columna o fila específica
+colnames(xCol)[5]
+colnames(xCol)[5] <- "CINCO"
+colnames(xCol)
+rm(xCol, xFila, xMat, xMatFila, x, y, z)
+
+# Data frames -------------------------------------------------------------
+#Creando un data frame vacio
+df <- data.frame()
+
+#Creando un data frame con varias columnas
+df <- data.frame(ID = c(1:5), 
+                 Nombres = c("Elena", "Isabela", "Juan", "Pedro", "Lola"),
+                 Edades = c(rep(30, 3), 15, 18))
+df
+dim(df)
+class(df)
+
+#Creando data frame basado en otros vectores
+ID <- c(1:5)
+Nombres <- c("Elena", "Isabela", "Juan", "Pedro", "Lola")
+Edades <- c(rep(30, 3), 15, 18)
+ID; Nombres; Edades
+#Uniendo todos los vectores
+df1 <- data.frame(ID, Nombres, Edades)
+df1
+
+#Comprobando si los dos data frames son iguales
+df == df1
+
+#Podemos cambiar los nombres de las columnas al crear el data frame con vectores
+colnames(df1)
+df1 <- data.frame(Identificacion = ID, Nombre = Nombres, Edad = Edades)
+df1
+#Reemplazando contenido de un elemento
+df1$Edad[3] <- NA
+df1
+
+#Tambien podemos cambiar los nombres despues de crear el data frame
+colnames(df)
+colnames(df) <- c("id", "nombre", "edad")
+colnames(df)
+
+#Accedemos elementos de la misma manera que con las matrices
+#Fila 1
+df[1,]
+#Columna 3 (edad)
+df[,3]
+#También podemos utilizar el nombre de la columna para acceder elementos del data frame
+df$edad
+df["edad"]
+#Fila 1, columna 2 (nombre)
+df[1, 2]
+df$nombre[1]
+df[1, "nombre"]
+
+
+# Factores ----------------------------------------------------------------
+#Nos permite agregar información de manera fácil
+Ciudad <- c(rep("Santa Cruz", 3), "Guayaquil", "Quito")
+Ciudad
+class(Ciudad)
+Ciudad <- factor(Ciudad)
+Ciudad
+class(Ciudad)
+levels(Ciudad)
+
+#Podemos agregar una columna adicional como lo hicimos con las matrices
+df <- cbind(df, Ciudad)
+df
+class(df)
+str(df)
+
+#Agregando/resumiendo información basado en la columna de factores (ciudad)
+table(df$Ciudad)
+table(df['Ciudad'])
+table(df[,4])
+
+
+# Listas ------------------------------------------------------------------
+Encuesta <- list(ID = ID,
+                 Nombres = Nombres,
+                 Participantes = 5,
+                 Casado = c(T, F, F, T, NA),
+                 Ciudad = Ciudad,
+                 Encuestador = c("Lisa", "Jorge"))
+Encuesta
+class(Encuesta)
+#Número de elementos incluidos en una lista
+length(Encuesta)
+
+#Acceder a elementos de una lista
+Encuesta$Participantes
+Encuesta[['Participantes']]
+Encuesta[3]
+
+#Acceder a un subelemento en una lista - Tercer elemento en Ciudad
+Encuesta$Ciudad[3]
+Encuesta[['Ciudad']][3]
+
+#Número de elementos dentro de un elemento de la lista
+length(Encuesta$Encuestador)
+length(Encuesta$Nombres)
+
+#Cambiar contenido de un elemento de la lista
+Encuesta$ID <- seq(1, 16, by = 3)
+Encuesta
+
+#Cambiar contenido de un sub-elemento de la lista
+Encuesta$Encuestador[2] <- "Pedro"
+Encuesta
