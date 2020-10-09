99ba227dd1a07b843ced4a031b40ab638ed6573b
Denisse Fierro Arcos <lidefi87@hotmail.com>
2020-10-09 18:59:36 -0500

    Incluyendo seccion de Maria Isabel Arrieta (R-Ladies Barranquilla)

--- Taller06_2020-10-10/ManipulacionVisualizacion-RLadiesGpsBarranquilla.R
+++ Taller06_2020-10-10/ManipulacionVisualizacion-RLadiesGpsBarranquilla.R
@@ -1,7 +1,8 @@
 ################### Taller de manipulacion y visualizacion de datos ###################
 #Organizado por: R-Ladies Barranquilla y R-Ladies Galapagos
 #Fecha del evento: 2020-10-10
-#Taller por: Denisse Fierro Arcos (R-Ladies Galapagos)
+#Taller por: Denisse Fierro Arcos (R-Ladies Galapagos) 
+#            Maria Isabel Arrieta Escobar (R-Ladies Barranquilla)
 
 # Llamando paquetes -------------------------------------------------------
 library(tidyverse)
@@ -9,6 +10,197 @@ library(tidyverse)
 #Puedes verificar los paquetes incluidos utilizando la siguiente linea
 tidyverse_packages()
 
+
+# Ejercicio 1. Comando basicos --------------------------------------------
+#Creamos la tabla del ejemplo con la funcion tribble----
+tribble(~Raza,      ~Indicador,  ~`2018`, ~`2019`, 
+        "Beagle", "Precio",     300.000,     350.000,
+        "Pastor Aleman",   "Precio",     400.000, 340.000,
+        "Bulldog", "Precio", 350.000, 400.000
+) -> desordenado      
+desordenado
+
+#----Ordenar datos I----
+desordenado %>% 
+  gather() #Empleamos la funcion gather () sin argumentos
+
+#----Ordenar datos II----
+desordenado %>% 
+  gather(key, value, #Los dos primeros argumentos son los nombres de las columnas que se crearon.
+         #Los siguientes las columnas que pasaremos a filas. Estos nombres de columna se repetiran en key y las filas llenaran value.  
+         '2018', '2019')  
+
+#----Ordenar datos III----
+desordenado %>% 
+  gather(Anio, Precio,            #Doy nombres utiles a key y value: quiero los anios y los precios. 
+         -Raza, -Indicador) %>%   #Especifico que columnas NO voy a pasar a filas usando "-".
+  select(-Indicador)
+
+#----Usar la gramatica de dplyr----
+tribble(                         #Creo los datos con la funcion tribble, muy util por su legibilidad al organizar la entrada en columnas. 
+  ~Col1,    ~Col2,        ~Col3, #Nombres de columna, se antepone ~
+  "Pepita", "Manzana",    48,    #Llenamos las columnas una a una. Mucha atencion a las comas!!
+  "Pepita", "Pera"   ,    52,
+  "Citrico", "Naranja",   12, 
+  "Citrico", "Mandarina", 8,
+  "Carozo",  "Durazno",   60,
+  "Carozo",  "Ciruela",   55     #En la ultima columna NO usamos la coma. 
+)                        #Cierro el parentesis de la funcion tribble. Se genera el data.frame. 
+                         #Silenciosamente invoca a la funcion print() y saca el resultado en consola.
+
+#----Renombrar columnas----
+tribble(
+  ~Col1,    ~Col2,        ~Col3, 
+  "Pepita", "Manzana",    48,     
+  "Pepita", "Pera"   ,    52,
+  "Citrico", "Naranja",   12, 
+  "Citrico", "Mandarina", 8,
+  "Carozo",  "Durazno",   60,
+  "Carozo",  "Ciruela",   55
+)  %>%                   #Paso el data.frame a la siguiente funcion.
+  rename(Tipo = Col1,              #Cambio los nombres a unos mas faciles de recordar.
+         Fruta = Col2,             #Hago un cambio en cada linea para facilitar la legibilidad. 
+         Precio = Col3) 
+
+#----Filtrar filas----
+tribble(
+  ~Col1,    ~Col2,        ~Col3, 
+  "Pepita", "Manzana",    48,     
+  "Pepita", "Pera"   ,    52,
+  "Citrico", "Naranja",   12, 
+  "Citrico", "Mandarina", 8,
+  "Carozo",  "Durazno",   60,
+  "Carozo",  "Ciruela",   55
+)  %>%                   
+  rename(Tipo = Col1,              
+         Fruta = Col2,             
+         Precio = Col3) %>% 
+  #Conserva solamente las filas en las que Tipo es igual a "Citrico" 
+  filter(Tipo == "Citrico") 
+
+#----Filtrado negativo.----
+tribble(
+  ~Col1,    ~Col2,        ~Col3, 
+  "Pepita", "Manzana",    48,     
+  "Pepita", "Pera"   ,    52,
+  "Citrico", "Naranja",   12, 
+  "Citrico", "Mandarina", 8,
+  "Carozo",  "Durazno",   60,
+  "Carozo",  "Ciruela",   55
+)  %>%                   
+  rename(Tipo = Col1,              
+         Fruta = Col2,             
+         Precio = Col3) %>% 
+  #Usando != conserva solamente las filas en las que Tipo NO es igual a "Citrico"
+  filter(Tipo != "Citrico")    
+
+#----Filtrado para datos numericos. filter()----
+tribble(
+  ~Col1,    ~Col2,        ~Col3, 
+  "Pepita", "Manzana",    48,     
+  "Pepita", "Pera"   ,    52,
+  "Citrico", "Naranja",   12, 
+  "Citrico", "Mandarina", 8,
+  "Carozo",  "Durazno",   60,
+  "Carozo",  "Ciruela",   55
+)  %>%                   
+  rename(Tipo = Col1,              
+         Fruta = Col2,             
+         Precio = Col3) %>% 
+  #Usando >= conservo solamente las filas en las que el precio es mayor a igual a 12. <, >, y <= tambien son validos.
+  filter(Precio >= 12)    
+
+#----Seleccionar columnas select()----
+tribble(
+  ~Col1,    ~Col2,       ~Col3, 
+  "Pepita", "Manzana",   48,     
+  "Pepita", "Pera"   ,   52,
+  "Citrico","Naranja",   12, 
+  "Citrico","Mandarina", 8,
+  "Carozo", "Durazno",   60,
+  "Carozo", "Ciruela",   55
+)  %>%                   
+  rename(Tipo = Col1,              
+         Fruta = Col2,             
+         Precio = Col3) %>% 
+  #Filtro los que NO son citricos usando != en lugar de ==
+  filter(Tipo != "Citrico")  %>%   
+  #Selecciono las columnas Precio y Fruta, saldran en ese orden. Notese que cada funcion opera sobre el output de la anterior.
+  select(Precio, Fruta)
+
+#----Calculos agrupados ----
+tribble(
+  ~Col1,    ~Col2,        ~Col3, 
+  "Pepita", "Manzana",    48,     
+  "Pepita", "Pera"   ,    52,
+  "Citrico", "Naranja",   12, 
+  "Citrico", "Mandarina", 8,
+  "Carozo",  "Durazno",   60,
+  "Carozo",  "Ciruela",   55
+)  %>%                   
+  rename(Tipo = Col1,              
+         Fruta = Col2,             
+         Precio = Col3) %>%        #Retomo los datos originales, renombradas las columnas. 
+  #Agrupo por tipo. Las siguientes operaciones se haran por este grupo.
+  group_by(Tipo) %>%             
+  #Nombre de la columna que se creara con el sumario. Opcional, pero recomendable. 
+  summarise(promedio =            
+              #Funcion que utilizaremos, mean, regresa la media. 
+              mean(              
+                #Columna sobre la que opera la funcion.
+                Precio)
+            #Cierro el parentesis que abri en summarise()
+            )  %>%               
+  #Ordeno de menor a mayor.
+  arrange(promedio)
+
+
+# Transformaciones de columnas  -------------------------------------------
+tribble(
+  ~Col1,    ~Col2,        ~Col3, ~Col4, #Cree una nueva columna. 
+  "Pepita", "Manzana",    48,     12,   
+  "Pepita", "Pera"   ,    52,     7, 
+  "Citrico", "Naranja",   12,     50,
+  "Citrico", "Mandarina", 8,      40,
+  "Carozo",  "Durazno",   60,     3, 
+  "Carozo",  "Ciruela",   55,     10
+)  %>%
+  #La nueva columna son los kilos en el inventario.
+  rename(Tipo = Col1,             
+         Fruta = Col2,            
+         Precio = Col3,
+         Kilos = Col4) %>%            
+  #Recodifico una variable categorica. 
+  mutate(Fruta = recode(Fruta,          
+                        #categoria_vieja="categoria_nueva". Atencion a las comillas!
+                      Manzana = "Manzana Verde")) %>%
+  #Creo una nueva columna numerica a partir de otras dos.Uso * para multiplicacion. Tambien podria usar +, -, / y ^ para potencia.
+  mutate(Capital = Precio*Kilos)        
+
+#Transformaciones de columnas mutate ()
+tribble(
+  ~Col1,    ~Col2,        ~Col3, ~Col4, #Cree una nueva columna. 
+  "Pepita", "Manzana",    48,     12,   
+  "Pepita", "Pera"   ,    52,     7, 
+  "Citrico", "Naranja",   12,     50,
+  "Citrico", "Mandarina", 8,      40,
+  "Carozo",  "Durazno",   60,     3, 
+  "Carozo",  "Ciruela",   55,     10
+)  %>%                  
+  rename(Tipo = Col1,             
+         Fruta = Col2,            
+         Precio = Col3,
+         #La nueva columna son los kilos en el inventario.
+         Kilos = Col4) %>%
+  #Recodifico una variable categorica. 
+  mutate(Fruta = recode(Fruta,
+                        #categoria_vieja="categoria_nueva".  Atencion a las comillas!
+                        Manzana = "Manzana Verde")) %>%
+  #Creo una nueva columna numerica a partir de otras dos.Uso * para multiplicacion. Tambien podria usar +, -, / y ^ para potencia.
+  mutate(Capital = Precio*Kilos)        
+
+
+#----Ejercicio 2. Empleo de los comando basicos empleando una base de datos mas compleja----
 # Accediendo datos sobre pinguinos ----------------------------------------
 #Podemos acceder a bases de datos que estan publicadas en linea con read_csv()
 pinguinos <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-07-28/penguins_raw.csv')
@@ -151,3 +343,5 @@ PingLimp %>%
        colour = "Sexo", shape = "Especies", size = "Peso (Kg)")+
   theme_bw()+
   facet_grid(island~.)
+
+
