99845411eb2e652928cb5079c2cc193e5ebe0f68
lidefi87 <lidefi87@hotmail.com>
2021-06-19 22:13:53 +1000

    Sesion 4 - Club de lectura R4DS

--- ClubLectura_R4DS_Sesion4_2021-06-24/Scripts/ClubLectura_R4DS_Sesion4_Cap21.R
+++ ClubLectura_R4DS_Sesion4_2021-06-24/Scripts/ClubLectura_R4DS_Sesion4_Cap21.R
@@ -139,6 +139,11 @@ ping %>%
   select(bill_length_mm:body_mass_g) %>% 
   map_dbl(mean, na.rm = T)
 
+#Otra opcion es escoger todas columnas con numeros decimales
+ping %>% 
+  select_if(is.double) %>% 
+  map_dbl(mean, na.rm = T)
+
 #Comparamos con los resultados anteriores
 prom3; prom2; unlist(prom1); prom
 
@@ -147,4 +152,229 @@ map_dbl(ping, mean, na.rm = T)
 #Simplemente no obtenemos resultados para columnas no numericas y una advertencia
 
 
+#Asumamos que quisieramos calcular un modelo lineal que nos permita establecer si 
+#el largo de la aleta esta relacionada al peso de los pinguinos, pero queremos
+#aplicar este modelo a cada especie de pinguino
+modelos_especies <- ping %>% 
+  #Dividimos nuestros datos de acuerdo a la especie
+  split(.$species) %>% 
+  #Aplicamos los modelos lineales a cada especie por separado
+  map(~lm(body_mass_g ~ flipper_length_mm, data = .x))
+#Recuerda que map siempre espera una lista y regresa una lista
+
+#Veamos los resultados - utilizemos un for loop para automatizarlo
+for(i in names(modelos_especies)){
+  print(i)
+  print(summary(modelos_especies[[i]]))
+}
+
+#O incluso mejor, utilizamos map
+modelos_especies %>% 
+  map(summary) %>% 
+  #Podemos anadir una linea mas y extraemos el r^2 que nos da la correlacion entre
+  #variables
+  map_dbl("r.squared") #equivalente a map_dbl(~.x$r.squared)
+
+
+#Esto seria equivalente a utilizar el siguiente codigo por cada especie
+ping %>% 
+  filter(species == "Adelie") %$% 
+  lm(body_mass_g ~ flipper_length_mm) %>% 
+  summary
+
+
+# Lidiando con errores ----------------------------------------------------
+#Funcion safely - Utilicemosla en conjunto con la raiz cuadrada
+raizCuadrada <- safely(sqrt)
+
+#Aplicamos la funcion a una lista de numeros
+raizCuadrada(10)
+#Tenemos el resultado mas una lista de errores que en este caso esta vacia
+
+#Si la aplicamos a caracteres
+raizCuadrada("10")
+#Tenemos un resultado que esta vacia y una explicacion del error en la operacion
+
+glimpse(raizCuadrada("10"))
+
+#Podemos usar safely con las funciones map
+miLista <- list(1:3, 4, "x")
+miLista %>% 
+  map(raizCuadrada)
+#El codigo se evalua y nos indica donde hubo errores
+
+#Comparemos al resultado sin safely
+miLista %>% 
+  map(sqrt)
+#No es muy conveniente porque simplemente obtenemos una falla
+
+#Pero podemos mejorar un poco como se muestran los errores
+resultados <- miLista %>% 
+  map(raizCuadrada) %>% 
+  #Asi mostramos a los resultados primero y luego los errores
+  transpose()
+
+#Identificamos donde no existen errores - errores = NULL
+no_errores <- resultados$error %>% 
+  map_lgl(is_null)
+
+#Mostramos los elementos de la lista donde no fueron detectados errores
+resultados$result[no_errores] %>% 
+  flatten_dbl()
+
+#Otras funciones relevantes incluyen possibly y quietly - Comparemos
+#Safely
+miLista %>% 
+  map(safely(sqrt))
+
+#Possibly
+miLista %>% 
+  #Con possibly tenemos que incluir el error a ser mostrado
+  map(possibly(sqrt, "no es un numero"))
+
+#Quietly - no captura errores, pero si advertencias
+list(1, -1) %>% 
+  map(quietly(sqrt)) %>%
+  transpose()
+
+
+
+# Multiples argumentos con map --------------------------------------------
+#Supongamos que volvemos a la regresion lineal
+grupos <- ping %>% 
+  split(.$species)
+  
+resultados <- grupos %>% 
+  map(~lm(body_mass_g ~ flipper_length_mm, data = .))
+
+#Que pasa si quiero obtener predicciones, utilizo los resultados de la regresion
+#y los aplico a cada grupo
+predicciones <- map2(resultados, grupos, predict)
+predicciones
+
+#Esto es equivalente a
+seq_along(resultados) %>% 
+  map(~predict(resultados[[.x]], grupos[[.x]]))
+
+#O equivalente a repetir la siguiente linea de codigo por cada grupo
+predict(resultados$Gentoo, grupos$Gentoo)
+
+#O en un for loop
+for(i in seq_along(grupos)){
+  print(names(grupos)[i])
+  print(predict(resultados[[i]], grupos[[i]]))
+}
+
+#Si tenemos mas de dos argumentos, entonces usamos pmap
+df <- data.frame(
+  x = c("manzana", "banana", "cereza"),
+  pattern = c("m", "n", "z"),
+  replacement = c("M", "N", "Z"),
+  stringsAsFactors = FALSE
+)
+pmap(df, gsub)
+#En este caso, gsub usa tres argumentos, por lo que usamos pmap
+
+#Esto equivale a
+for(i in 1:nrow(df)){
+  print(gsub(df$pattern[i], df$replacement[i], df$x[i]))
+}
+  
+  
+# Aplicando distintas funciones a distintos datos -------------------------
+#Asumamos que queremos obtener el promedio del largo del pico, la mediana de la
+#aleta y el maximo del peso
+#Creamos una lista con los datos de interes
+datos_morfo <- list(list(x = ping$bill_length_mm, na.rm = T),
+                    list(x = ping$flipper_length_mm, na.rm = T),
+                    list(x = ping$body_mass_g, na.rm = T))
+
+#Luego creamos un vector o lista con las funciones de interes
+func <- list(mean, median, max)
+#Es necesario que la lista contenga el numero mismo de elementos que argumentos
+#necesarios para llevar a cabo la operacion
+
+#Aplicamos la operacion
+invoke_map(func, datos_morfo)
+
+#Comparemos con la opcion manual
+mean(ping$bill_length_mm, na.rm = T)
+median(ping$flipper_length_mm, na.rm = T)
+max(ping$body_mass_g, na.rm = T)
+
+
+#Alternativamente podemos crear una sola tribble
+datos_morfo2 <- tribble(~func, ~datos,
+                        mean, list(x = ping$bill_length_mm, na.rm = T),
+                        median, list(x = ping$flipper_length_mm, na.rm = T),
+                        max, list(x = ping$body_mass_g, na.rm = T))
+
+#Dentro del tribble creamos una nueva columna donde guardaremos los resultados
+#de las operaciones
+datos_morfo2 %>% 
+  mutate(datos_morfo = invoke_map(func, datos)) %>% 
+  #Seleccionamos la columna que acabamos de crear
+  select(datos_morfo) %>% 
+  #Extraemos la informacion de la lista para mostrarla como un vector
+  flatten() %>% 
+  flatten_dbl()
+
+
+# Funcion Walk ------------------------------------------------------------
+#Creemos una figura de puntos para cada especie de pinguinos
+fig <- ping %>% 
+  split(.$species) %>% 
+  map(~ggplot(.x, aes(bill_length_mm, body_mass_g, col = sex)) + geom_point())
+
+#Creemos los nombres de la figura con la extension pdf
+nombres <- str_c(names(fig), ".pdf")
+
+#Guardemos las figuras en la carpeta Figuras
+pwalk(list(nombres, fig), ggsave, path = "ClubLectura_R4DS_Sesion4_2021-06-24/Figuras/")
+
+
+# Otras funciones utiles --------------------------------------------------
+#Keep mantiene las columnas que cumplen una condicion
+ping %>% 
+  keep(is.double)
+
+#Discard descarta las columnas que cumplen una condicion
+ping %>% 
+  discard(is.integer)
+
+#Some determinan si algunos elementos cumplen una condicion
+ping %>% 
+  flatten() %>% 
+  some(is.double)
+
+#Every verifica si todos los elementos cumplen una condicion
+ping %>% 
+  flatten() %>% 
+  every(is.double)
+
+
+#Reduciendo listas basadas en elementos comunes
+#Creemos un identificador unico
+ping2 <- ping %>% 
+  rowid_to_column("id") %>% 
+  unite("id", species, id)
+
+#Creamos una lista con el identificador unico y unas columnas adicionales
+df <- list(isla = tibble(id = ping2$id, isla = ping$island),
+           sexo = tibble(id = ping2$id, sexo = ping$sex),
+           peso = tibble(id = ping2$id, peso = ping$body_mass_g))
+df
+
+#Creemos un solo data frame con los datos por cada identificador unico unificados
+df <- df %>% 
+  reduce(full_join)
+df
+
+
+#Si queremos encontrar los pesos que se repiten entre hembras y machos
+pesos_comunes <- list(peso_hembras = ping$body_mass_g[ping$sex == "female"],
+                      peso_machos = ping$body_mass_g[ping$sex == "male"])
+#Encontremos los pesos comunes
+pesos_comunes %>% 
+  reduce(intersect)
 
